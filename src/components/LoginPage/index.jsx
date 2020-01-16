25d654fbcff6e839f5b0ff15dd6857be274e7f8e
rostyk1331 <khanasrostyslav@gmail.com>
2020-01-16 18:08:36 +0200

    add cognito config

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import logo from './logo.png';
+import { Auth } from 'aws-amplify';
 
 class LoginPage extends React.Component {
+    state = { email: '', password: '' };
+
+    onEmailChange = (e) => {
+        this.setState({email: e.target.value});
+    }
+
+    onPasswordChange = (e) => {
+        this.setState({password: e.target.value});
+    }
+
+    onSignIn = async () => {
+        // const { email, password } = this.state;
+        // const user = await Auth.signIn(email, password);
+        // console.log(user);
+    }
+
+    onGoogleSignIn = async () => {
+        // const token = await Auth.federatedSignIn({ provider: "Google" });
+        // console.log(token);
+    }
+
     render() {
+        const { email, password } = this.state;
         return (
             <div>
-                LoginPage
+                <header>
+                    <img src={logo}/>
+                    <h1>Register for Tourney Master</h1>
+                </header>
+                <form>
+                    <button type="button" onClick={this.onGoogleSignIn}>Sign in with Google</button>
+                    <p>OR</p>
+                    <label>Email Adddress*</label>
+                    <input type='text' value={email} onChange={this.onEmailChange}/>
+                    <label>Password*</label>
+                    <input type='text'value={password} onChange={this.onPasswordChange}/>
+                    <button type="button" onClick={this.onSignIn}>Sign In</button>
+                </form>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Amplify from 'aws-amplify';
+import config from './config';
+
+Amplify.configure({
+    Auth: {
+        mandatorySignId: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+        oauth: {
+            domain: config.cognito.DOMAIN,
+            redirectSignIn: 'http://localhost:3000/',
+            redirectSignOut: 'http://localhost:3000/',
+            responseType: 'token'
+        }
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
