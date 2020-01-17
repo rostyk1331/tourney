25daafeb61da10507fe9fe3caba36f59128fd74c
rostyk1331 <khanasrostyslav@gmail.com>
2020-01-17 11:36:53 +0200

    styles fixes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import logo from '../LoginPage/logo.png';
 import avatar from '../Header/avatar.png';
-import './style.css';
+import styles from './style.module.scss';
 
 class Header extends React.Component {
     render() {
         return (
-            <ul className="navbar">
-                <li><img src={logo} className="logo-header"/></li>
-                <div className="menu-container">
+            <ul className={styles['navbar']}>
+                <li><img src={logo} className={styles["logo-header"]}/></li>
+                <div className={styles["menu-container"]}>
                     <li><a href="#">Event Production</a></li>
                     <li><a href="#">Event Search</a></li>
                     <li><a href="#">Support</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                 </div>
-                <li><img src={avatar} className="avatar"/></li>
+                <li><img src={avatar} className={styles["avatar"]}/></li>
             </ul>
         )
     }
